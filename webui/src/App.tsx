723d570349703c3958686a907900ff5886a7480e
Andrew Baldwin <andrew.baldwin44@gmail.com>
2024-08-26 10:35:22 -0400

    Move toolbar

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -1,24 +1,10 @@
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import {
-  SWARM_STATE,
-  SwarmForm,
-  tabConfig,
-  Tabs,
-  useCreateTheme,
-  useFetchExceptions,
-  useFetchStats,
-  useFetchTasks,
-  useLogViewer,
-} from 'locust-ui';
+import LocustUi, { tabConfig } from 'locust-ui';
 
-import Layout from 'components/Layout/Layout';
 import Charts from 'components/tabs/Charts';
 import Scatterplot from 'components/tabs/Scatterplot';
 import Stats from 'components/tabs/Stats';
 import Testruns from 'components/tabs/Testruns';
 import useFetchTestruns from 'hooks/useFetchTestruns';
-import { useLocustSelector } from 'redux/hooks';
 
 const tabs = [
   {
@@ -49,20 +35,7 @@ const tabs = [
 ];
 
 export default function App() {
-  useFetchStats();
-  useFetchExceptions();
-  useFetchTasks();
-  useLogViewer();
   useFetchTestruns();
 
-  const swarmState = useLocustSelector(({ swarm }) => swarm.state);
-
-  const theme = useCreateTheme();
-
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Layout>{swarmState === SWARM_STATE.READY ? <SwarmForm /> : <Tabs tabs={tabs} />}</Layout>
-    </ThemeProvider>
-  );
+  return <LocustUi tabs={tabs} />;
 }
