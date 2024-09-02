6d31ba39979f55875c7386f7aa7cf8f5d08b52d1
Lars Holmberg <lars.holmberg@redshirt.se>
2024-09-02 21:25:04 +0200

    print error output...

--- hatch_build.py
+++ hatch_build.py
@@ -5,6 +5,6 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class BuildFrontend(BuildHookInterface):
     def initialize(self, version, build_data):
-        subprocess.check_output("yarn build", cwd="locust_cloud/webui", shell=True)
+        subprocess.check_output("yarn build", cwd="locust_cloud/webui", stderr=subprocess.STDOUT, shell=True)
 
         return super().initialize(version, build_data)
