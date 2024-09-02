34ec6523c086b28244187773fd8944f56f740098
Lars Holmberg <lars.holmberg@redshirt.se>
2024-09-02 22:52:18 +0200

    cleanup

--- hatch_build.py
+++ hatch_build.py
@@ -5,10 +5,9 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class BuildFrontend(BuildHookInterface):
     def initialize(self, version, build_data):
-        
         try:
             subprocess.check_output("yarn build", cwd="locust_cloud/webui", shell=True, stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as e:
-            raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+            raise RuntimeError(f"'{e.cmd}' got exit code {e.returncode}: {e.output}")
 
         return super().initialize(version, build_data)
