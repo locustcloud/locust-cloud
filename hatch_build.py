e5e0f13ccb7ebcc00af6f7fd40836ddf5b4fc084
Lars Holmberg <lars.holmberg@redshirt.se>
2024-09-02 21:25:04 +0200

    better error message if yarn fails

--- hatch_build.py
+++ hatch_build.py
@@ -5,6 +5,9 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class BuildFrontend(BuildHookInterface):
     def initialize(self, version, build_data):
-        subprocess.check_output("yarn build", cwd="locust_cloud/webui", shell=True)
+        try:
+            subprocess.check_output("yarn build", cwd="locust_cloud/webui", shell=True, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError(f"'{e.cmd}' got exit code {e.returncode}: {e.output}")
 
         return super().initialize(version, build_data)
