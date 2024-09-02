affd0173a13bd981c87441f16935ccd7ff4f40ee
Lars Holmberg <lars.holmberg@redshirt.se>
2024-09-02 22:23:22 +0200

    Update hatch_build.py

--- hatch_build.py
+++ hatch_build.py
@@ -5,6 +5,10 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class BuildFrontend(BuildHookInterface):
     def initialize(self, version, build_data):
-        subprocess.check_output("yarn build", cwd="locust_cloud/webui", stderr=subprocess.STDOUT, shell=True)
+        
+        try:
+            subprocess.check_output("yarn build", cwd="locust_cloud/webui",shell=True,stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output)
 
         return super().initialize(version, build_data)
