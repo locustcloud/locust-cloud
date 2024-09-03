e9f72019e80505b6efbc85bc361520494a243c47
Lars Holmberg <lars.holmberg@redshirt.se>
2024-09-03 09:26:41 +0200

    Run yarn install as part of build

--- hatch_build.py
+++ hatch_build.py
@@ -6,6 +6,7 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 class BuildFrontend(BuildHookInterface):
     def initialize(self, version, build_data):
         try:
+            subprocess.check_output("yarn install", cwd="locust_cloud/webui", shell=True, stderr=subprocess.STDOUT)
             subprocess.check_output("yarn build", cwd="locust_cloud/webui", shell=True, stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as e:
             raise RuntimeError(f"'{e.cmd}' got exit code {e.returncode}: {e.output}")
