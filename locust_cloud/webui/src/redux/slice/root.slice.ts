43cc276682d2b2ac3a73b8c99fe5f356b233f6ba
Andrew Baldwin <andrew.baldwin44@gmail.com>
2024-09-30 09:29:04 +0200

    Add classic cloud view toggle

--- locust_cloud/webui/src/redux/slice/root.slice.ts
+++ locust_cloud/webui/src/redux/slice/root.slice.ts
@@ -3,17 +3,19 @@ import { combineReducers } from '@reduxjs/toolkit';
 import { ReactReduxContextValue } from 'react-redux';
 
 import toolbar, { IToolbarState, ToolbarAction } from 'redux/slice/toolbar.slice';
+import ui, { IUiState, UiAction } from 'redux/slice/ui.slice';
 
 export interface IRootState {
   toolbar: IToolbarState;
+  ui: IUiState;
 }
 
-export type Action = ToolbarAction;
-
+export type Action = ToolbarAction | UiAction;
 export const ReduxContext = createContext<ReactReduxContextValue<IRootState, any> | null>(null);
 
 const rootReducer = combineReducers({
   toolbar,
+  ui,
 });
 
 export default rootReducer;
