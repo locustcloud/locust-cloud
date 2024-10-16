3e00854815f4b167eabcc9799ecb4228fd37b5ef
Andrew Baldwin <andrew.baldwin44@gmail.com>
2024-10-16 13:06:30 +0200

    Fix viewer

--- locust_cloud/webui/src/components/tabs/Charts.tsx
+++ locust_cloud/webui/src/components/tabs/Charts.tsx
@@ -91,7 +91,6 @@ export default function Charts() {
   const [getRps] = useGetRpsMutation();
 
   const fetchCharts = async () => {
-    console.log({ currentTestrun });
     if (currentTestrun) {
       const currentTimestamp = new Date().toISOString();
       const { endTime } = testruns[new Date(currentTestrun).toLocaleString()];
@@ -150,8 +149,6 @@ export default function Charts() {
         responseLength,
         rpsData,
       });
-      console.log('here');
-      console.log({ requestLines });
 
       setIsLoading(false);
 
