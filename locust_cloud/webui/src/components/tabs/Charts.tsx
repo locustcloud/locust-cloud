75e6164adb06d64f7225b8c74794d91b507ce418
Andrew Baldwin <andrew.baldwin44@gmail.com>
2024-10-21 16:06:29 +0200

    Add show / hide advanced

--- locust_cloud/webui/src/components/tabs/Charts.tsx
+++ locust_cloud/webui/src/components/tabs/Charts.tsx
@@ -21,7 +21,6 @@ import { IPerRequestData, chartValueFormatter } from 'utils/api';
 
 const defaultPerRequestState = { time: [] } as IPerRequestData;
 const defaultRpsDataState = { time: [] as string[] } as IRpsData;
-const defaultRequestLines = [] as IRequestLines[];
 
 const CHART_COLORS = {
   RPS: ['#00ca5a', '#0099ff', '#ff6d6d'],
@@ -72,9 +71,8 @@ const defaultChartData = {
 
 export default function Charts() {
   const { state: swarmState } = useLocustSelector(({ swarm }) => swarm);
-  const { resolution, currentTestrunIndex, currentTestrun, testruns } = useSelector(
-    ({ toolbar }) => toolbar,
-  );
+  const { resolution, currentTestrunIndex, currentTestrun, testruns, shouldShowAdvanced } =
+    useSelector(({ toolbar }) => toolbar);
   const setSnackbar = useAction(snackbarActions.setSnackbar);
 
   const [isLoading, setIsLoading] = useState(true);
@@ -102,16 +100,16 @@ export default function Charts() {
       };
 
       const mutations = [
-        getRequestNames,
-        getRps,
-        getAvgResponseTimes,
-        getRpsPerRequest,
-        getErrorsPerRequest,
-        getPerc99ResponseTimes,
-        getResponseLength,
-      ];
+        { key: 'requestLines', mutation: getRequestNames },
+        { key: 'rpsData', mutation: getRps },
+        { key: 'avgResponseTimes', mutation: getAvgResponseTimes },
+        { key: 'rpsPerRequest', mutation: getRpsPerRequest, isAdvanced: true },
+        { key: 'errorsPerRequest', mutation: getErrorsPerRequest, isAdvanced: true },
+        { key: 'perc99ResponseTimes', mutation: getPerc99ResponseTimes, isAdvanced: true },
+        { key: 'responseLength', mutation: getResponseLength, isAdvanced: true },
+      ].filter(({ isAdvanced }) => !isAdvanced || shouldShowAdvanced);
 
-      const mutationResults = await Promise.all(mutations.map(mutation => mutation(payload)));
+      const mutationResults = await Promise.all(mutations.map(({ mutation }) => mutation(payload)));
 
       const fetchError = mutationResults.filter(({ error }) => error);
 
@@ -119,36 +117,22 @@ export default function Charts() {
         setSnackbar({ message: String(fetchError.error) });
       }
 
-      const [
-        { data: requestLines = defaultRequestLines },
-        { data: rpsData = defaultRpsDataState },
-        { data: avgResponseTimes = defaultPerRequestState },
-        { data: rpsPerRequest = defaultPerRequestState },
-        { data: errorsPerRequest = defaultPerRequestState },
-        { data: perc99ResponseTimes = defaultPerRequestState },
-        { data: responseLength = defaultPerRequestState },
-      ] = mutationResults;
+      const chartData = mutations.reduce(
+        (charts, { key }, index) => ({ ...charts, [key]: mutationResults[index].data }),
+        {} as ICharts,
+      );
 
       // only show an error for the first testrun if no test is running
       if (
-        (currentTestrunIndex !== 0 && !(requestLines as IRequestLines[]).length) ||
+        (currentTestrunIndex !== 0 && !chartData.requestLines.length) ||
         (currentTestrunIndex === 0 &&
           swarmState !== SWARM_STATE.RUNNING &&
-          !(requestLines as IRequestLines[]).length)
+          !chartData.requestLines.length)
       ) {
         setIsError(true);
       }
 
-      setCharts({
-        requestLines: requestLines as IRequestLines[],
-        rpsData: rpsData as IRpsData,
-        avgResponseTimes: avgResponseTimes as IPerRequestData,
-        rpsPerRequest: rpsPerRequest as IPerRequestData,
-        errorsPerRequest: errorsPerRequest as IPerRequestData,
-        perc99ResponseTimes: perc99ResponseTimes as IPerRequestData,
-        responseLength: responseLength as IPerRequestData,
-      });
-
+      setCharts({ ...charts, ...chartData });
       setIsLoading(false);
 
       if (swarmState !== SWARM_STATE.STOPPED) {
@@ -163,9 +147,9 @@ export default function Charts() {
   });
 
   useEffect(() => {
-    // handle initial load and fetching on testrun or resolution change
+    // handle initial load and fetching on testrun, resolution change, or showing of advanced charts
     fetchCharts();
-  }, [currentTestrun, resolution]);
+  }, [currentTestrun, resolution, shouldShowAdvanced]);
 
   useEffect(() => {
     if (swarmState === SWARM_STATE.RUNNING) {
@@ -180,7 +164,7 @@ export default function Charts() {
 
   return (
     <>
-      <Toolbar onSelectTestRun={onSelectTestRun} />
+      <Toolbar onSelectTestRun={onSelectTestRun} showHideAdvanced />
       {isError && (
         <Alert severity='error'>There was a problem loading some graphs for this testrun.</Alert>
       )}
@@ -225,34 +209,38 @@ export default function Charts() {
             lines={charts.requestLines}
             title='Average Response Times'
           />
-          <LineChart<IPerRequestData>
-            chartValueFormatter={chartValueFormatter}
-            charts={charts.rpsPerRequest}
-            colors={CHART_COLORS.PER_REQUEST}
-            lines={charts.requestLines}
-            title='RPS per Request'
-          />
-          <LineChart<IPerRequestData>
-            chartValueFormatter={chartValueFormatter}
-            charts={charts.errorsPerRequest}
-            colors={CHART_COLORS.ERROR}
-            lines={charts.requestLines}
-            title='Errors per Request'
-          />
-          <LineChart<IPerRequestData>
-            chartValueFormatter={chartValueFormatter}
-            charts={charts.perc99ResponseTimes}
-            colors={CHART_COLORS.PER_REQUEST}
-            lines={charts.requestLines}
-            title='99th Percentile Response Times'
-          />
-          <LineChart<IPerRequestData>
-            chartValueFormatter={chartValueFormatter}
-            charts={charts.responseLength}
-            colors={CHART_COLORS.PER_REQUEST}
-            lines={charts.requestLines}
-            title='Response Length'
-          />
+          {shouldShowAdvanced && (
+            <>
+              <LineChart<IPerRequestData>
+                chartValueFormatter={chartValueFormatter}
+                charts={charts.rpsPerRequest}
+                colors={CHART_COLORS.PER_REQUEST}
+                lines={charts.requestLines}
+                title='RPS per Request'
+              />
+              <LineChart<IPerRequestData>
+                chartValueFormatter={chartValueFormatter}
+                charts={charts.errorsPerRequest}
+                colors={CHART_COLORS.ERROR}
+                lines={charts.requestLines}
+                title='Errors per Request'
+              />
+              <LineChart<IPerRequestData>
+                chartValueFormatter={chartValueFormatter}
+                charts={charts.perc99ResponseTimes}
+                colors={CHART_COLORS.PER_REQUEST}
+                lines={charts.requestLines}
+                title='99th Percentile Response Times'
+              />
+              <LineChart<IPerRequestData>
+                chartValueFormatter={chartValueFormatter}
+                charts={charts.responseLength}
+                colors={CHART_COLORS.PER_REQUEST}
+                lines={charts.requestLines}
+                title='Response Length'
+              />
+            </>
+          )}
         </Box>
       </Box>
     </>
