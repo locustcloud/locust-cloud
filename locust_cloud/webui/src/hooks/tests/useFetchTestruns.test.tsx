4b1bc423702933fb0ca0b42e29d64c48aa20f07f
Andrew Baldwin <andrew.baldwin44@gmail.com>
2024-12-03 14:42:46 -0500

    useFetchTestruns

--- locust_cloud/webui/src/hooks/tests/useFetchTestruns.test.tsx
+++ locust_cloud/webui/src/hooks/tests/useFetchTestruns.test.tsx
@@ -8,7 +8,11 @@ import useFetchTestruns from 'hooks/useFetchTestruns';
 import { TEST_BASE_API } from 'test/constants';
 import { renderWithProvider } from 'test/testUtils';
 
-const mockRunIds = ['2024-11-30 10:09:10.771147+00:00', '2024-10-30 05:58:31.234505+00:00'];
+const mockRunIds = [
+  '2024-11-30 10:09:10.771147+00:00',
+  '2024-10-30 05:58:31.234505+00:00',
+  '2024-10-30 06:00:44.325405+00:00',
+];
 const mockTestrunsForDisplay = mockRunIds.map(runId => new Date(runId).toLocaleString());
 const mockTestrunsResponse = [
   {
@@ -23,6 +27,12 @@ const mockTestrunsResponse = [
     profile: null,
     runId: mockRunIds[1],
   },
+  {
+    endTime: '2024-10-30 08:39:22.358997+00:00',
+    locustfile: 'different_locustfile.py',
+    profile: 'myprofile',
+    runId: mockRunIds[2],
+  },
 ];
 const mockTestruns = {
   [mockTestrunsForDisplay[0]]: {
@@ -34,6 +44,11 @@ const mockTestruns = {
     runId: mockRunIds[1],
     endTime: mockTestrunsResponse[1].endTime,
     index: 1,
+  },
+  [mockTestrunsForDisplay[2]]: {
+    runId: mockRunIds[2],
+    endTime: mockTestrunsResponse[2].endTime,
+    index: 2,
   },
 };
 
@@ -69,5 +84,111 @@ describe('useFetchStats', () => {
 
     expect(store.getState().toolbar.testrunsForDisplay).toEqual(mockTestrunsForDisplay);
     expect(store.getState().toolbar.testruns).toEqual(mockTestruns);
+    expect(store.getState().toolbar.currentTestrunIndex).toBe(0);
+    expect(store.getState().toolbar.currentTestrun).toBe(mockRunIds[0]);
+  });
+
+  test('should fetch testruns if isGraphViewer', async () => {
+    window.templateArgs.isGraphViewer = true;
+    const { store } = renderWithProvider(<MockHook />);
+    window.templateArgs.isGraphViewer = false;
+
+    await act(async () => {
+      await vi.advanceTimersByTimeAsync(500);
+    });
+
+    expect(store.getState().toolbar.testrunsForDisplay).toEqual(mockTestrunsForDisplay);
+  });
+
+  test('should set the current testrun according to url if isGraphViewer', async () => {
+    window.templateArgs.isGraphViewer = true;
+    const { store } = renderWithProvider(<MockHook />, undefined, {
+      url: { query: { testrun: mockTestrunsForDisplay[1] } },
+    });
+    window.templateArgs.isGraphViewer = false;
+
+    await act(async () => {
+      await vi.advanceTimersByTimeAsync(500);
+    });
+
+    expect(store.getState().toolbar.currentTestrunIndex).toBe(1);
+    expect(store.getState().toolbar.currentTestrun).toBe(mockRunIds[1]);
+  });
+
+  test('should fetch testruns if hasDismissedSwarmForm', async () => {
+    const { store } = renderWithProvider(<MockHook />, { ui: { hasDismissedSwarmForm: true } });
+
+    await act(async () => {
+      await vi.advanceTimersByTimeAsync(500);
+    });
+
+    expect(store.getState().toolbar.testrunsForDisplay).toEqual(mockTestrunsForDisplay);
+  });
+
+  test('should set the current testrun according to url if hasDismissedSwarmForm', async () => {
+    const { store } = renderWithProvider(
+      <MockHook />,
+      { ui: { hasDismissedSwarmForm: true } },
+      {
+        url: { query: { testrun: mockTestrunsForDisplay[1] } },
+      },
+    );
+
+    await act(async () => {
+      await vi.advanceTimersByTimeAsync(500);
+    });
+
+    expect(store.getState().toolbar.currentTestrunIndex).toBe(1);
+    expect(store.getState().toolbar.currentTestrun).toBe(mockRunIds[1]);
+  });
+
+  test('should filter testrunsForDisplay by locustfile', async () => {
+    const { store } = renderWithProvider(
+      <MockHook />,
+      { toolbar: { profile: mockTestrunsResponse[0].locustfile, testrunsForDisplay: [] } },
+      {
+        swarm: { state: SWARM_STATE.RUNNING },
+      },
+    );
+
+    await act(async () => {
+      await vi.advanceTimersByTimeAsync(500);
+    });
+
+    expect(store.getState().toolbar.testrunsForDisplay.length).toEqual(2);
+  });
+
+  test('should filter testrunsForDisplay by profile', async () => {
+    const { store } = renderWithProvider(
+      <MockHook />,
+      { toolbar: { profile: mockTestrunsResponse[2].profile, testrunsForDisplay: [] } },
+      {
+        swarm: { state: SWARM_STATE.RUNNING },
+      },
+    );
+
+    await act(async () => {
+      await vi.advanceTimersByTimeAsync(500);
+    });
+
+    expect(store.getState().toolbar.testrunsForDisplay.length).toEqual(1);
+    expect(store.getState().toolbar.currentTestrunIndex).toEqual(2);
+    expect(store.getState().toolbar.currentTestrun).toEqual(mockTestrunsResponse[2].runId);
+  });
+
+  test('should filter testrunsForDisplay by profile from url', async () => {
+    const { store } = renderWithProvider(
+      <MockHook />,
+      { ui: { hasDismissedSwarmForm: true } },
+      {
+        url: { query: { profile: mockTestrunsResponse[2].profile } },
+      },
+    );
+
+    await act(async () => {
+      await vi.advanceTimersByTimeAsync(500);
+    });
+
+    expect(store.getState().toolbar.testrunsForDisplay.length).toEqual(1);
   });
 });
