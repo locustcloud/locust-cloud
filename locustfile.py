3e35b4fc7d6820c709b59c8bf244bbe9b746c530
Lars Holmberg <lars@locust.cloud>
2025-09-15 12:16:43 +0200

    simplify locustfile

--- locustfile.py
+++ locustfile.py
@@ -1,18 +1,13 @@
-# Simple locustfile for testing against example target
-import pathlib
 import random
 
 from locust import FastHttpUser, run_single_user, task  # pyright: ignore [reportMissingImports]
 
-from testdata.autodetected import stuff
-
 product_ids = [1, 2, 42, 4711]
 
 
-stuff()
-
-
 class MyUser(FastHttpUser):
+    host = "https://mock-test-target.eu-north-1.locust.cloud"
+
     @task
     def t(self) -> None:
         with self.rest("POST", "/authenticate", json={"username": "foo", "password": "bar"}) as resp:
@@ -26,25 +21,6 @@ class MyUser(FastHttpUser):
         with self.rest("POST", "/checkout/confirm") as resp:
             if not resp.js.get("orderId"):
                 resp.failure("orderId missing")
-
-
-extra = pathlib.Path("testdata/extra-files/extra.txt")
-if extra.exists():
-    print("--extra-files verification:", extra.read_text())
-
-try:
-    import example  # type: ignore
-
-    example.hello()
-except ImportError:
-    pass  # ignore this for local runs
-
-try:
-    import dotenv  # type: ignore # noqa: F401
-
-    print("dotenv imported successfully, --requirements seems to be working")
-except ImportError:
-    pass  # ignore this for local runs
 
 
 if __name__ == "__main__":
