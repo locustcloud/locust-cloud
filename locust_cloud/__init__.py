c06ac3b5fbebcf5dfabf94d58a9b4751d6b9f61e
Andrew Baldwin <andrew.baldwin44@gmail.com>
2025-09-29 10:59:02 +0200

    Don't pass user count if no arg

--- locust_cloud/__init__.py
+++ locust_cloud/__init__.py
@@ -98,7 +98,6 @@ def main(locustfiles: list[str] | None = None):
         payload = {
             "locust_args": locust_args,
             "project_data": project_data,
-            "user_count": options.users,
         }
 
         if options.image_tag is not None:
@@ -110,6 +109,9 @@ def main(locustfiles: list[str] | None = None):
 
         if options.workers is not None:
             payload["worker_count"] = options.workers
+
+        if options.users:
+            payload["user_count"] = options.users
 
         if options.requirements:
             payload["requirements"] = options.requirements
